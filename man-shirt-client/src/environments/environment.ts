3137966a9578d25d8c495aab09086feddd025ccb
hieunm <hieunm156@gmail.com>
2025-11-06 15:40:16 +0700

    update

--- man-shirt-client/src/environments/environment.ts
+++ man-shirt-client/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: "https://api.newmenstore.com",
-  apiUrl: "http://103.162.20.122:8080",
+  apiUrl: "http://localhost:8080",
   instagram_token: "INSTA_TOKEN",
   stripe_token: "STRIPE_TOKEN",
   paypal_token: "PAYPAL_TOKEN",
